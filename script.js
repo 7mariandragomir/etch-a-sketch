fb228bb16cc67054b259abcc69521b4051710207
Marian <7mariandragomir@gmail.com>
2024-10-28 23:12:16 +0200

    added slider and color btns

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+let body = document.querySelector("body");
 let container = document.querySelector("#container");
+let color = "white"; 
+
 
 // the number of boxes in the grid. N => N/N;
-let numOfBoxes = 50;
+let numOfBoxes = 24;
 let boxSize = container.clientWidth / numOfBoxes; 
 
 
+// function to reset container 
+function resetContainer(cont) {
+    let children = cont.childELementCount;
+    while(cont.firstChild) {
+        cont.removeChild(cont.lastChild);
+    }
+}
+
+// function to draw the container
 function drawContainer() {
-    container.innerHTML = ''; 
+    resetContainer(container);
+    container.style.width = boxSize * numOfBoxes; 
+    boxSize = container.clientWidth / numOfBoxes; 
     for (let i = 0; i < (numOfBoxes * numOfBoxes); i++) {
-        container.style.width = boxSize * numOfBoxes; 
         const gridBox = document.createElement("div");
         gridBox.classList.add('grid-box');
         gridBox.style.width = boxSize.toString() + 'px';
@@ -16,50 +29,96 @@ function drawContainer() {
         
         container.appendChild(gridBox);
     }
-
+    startListeners()
 }
 
 drawContainer()
 
-const gridBoxes = document.querySelectorAll(".grid-box");
+// function to start all listners
+function startListeners() {
+    const gridBoxes = document.querySelectorAll(".grid-box");
 
-// Event Listners for the container 
-let isLeftClick = false;
-container.addEventListener("mousedown", (e) =>{
-    if (e.button === 0) {
-        isLeftClick = true;
-    }
-})
-
-container.addEventListener("mouseup", () => {
-    isLeftClick = false;
-})
-
-// Event Listeners for the boxes
-gridBoxes.forEach((box) => {
-    // color the box if you hover while holding left-click
-    box.addEventListener("mouseenter", () => {
-        if(isLeftClick) {
-            box.style.backgroundColor = "blue";
-            box.style.transition = ".5s";
+    // Event Listners for the container 
+    let isLeftClick = false;
+    container.addEventListener("mousedown", (e) =>{
+        if (e.button === 0) {
+            isLeftClick = true;
         }
     })
-    // color the box on single click
-    box.addEventListener("click", () => {
-        box.style.backgroundColor = "blue";
-        box.style.transition = ".5s";
+    
+    container.addEventListener("mouseup", () => {
+        isLeftClick = false;
     })
-})
-
+    
+    // Event Listeners for the boxes
+    gridBoxes.forEach((box) => {
+        // color the box if you hover while holding left-click
+        box.addEventListener("mouseenter", () => {
+            if(isLeftClick) {
+                box.style.backgroundColor = color;
+                box.style.transition = ".5s";
+            }
+        })
+        // color the box on single click
+        box.addEventListener("click", () => {
+            box.style.backgroundColor = color;
+            box.style.transition = ".5s";
+        })
+    })
+}
 
 // Slider 
 const slider = document.querySelector("#container-slider");
+const sliderBtn = document.querySelector("#set-container-size");
 let sliderDisplayVal = document.querySelector("#slider-value");
-let sliderVal;
+let sliderVal = numOfBoxes;
 
 sliderDisplayVal.innerHTML = numOfBoxes;
+slider.value = numOfBoxes;
 
 slider.addEventListener("input", () => {
     sliderDisplayVal.innerHTML = slider.value;
+    sliderVal = slider.value;
 })
+
+sliderBtn.addEventListener("click", () => {
+    numOfBoxes = Number(sliderVal);
+    drawContainer();
+})
+
+// Colors
+// const colorWhite = document.querySelector("#white");
+// const colorBlue = document.querySelector("#blue");
+// const colorRed = document.querySelector("#red");
+// const colorEraser = document.querySelector("#eraser");
+const colorBtns = document.querySelector("#color-btns");
+
+colorBtns.addEventListener("click", (e) => {
+    selectedColor = e.target.id;
+    switch(selectedColor) {
+        case "white": {
+            color = "#fff";
+            break};
+        case "blue": {
+            color = "rgb(0, 119, 255)";
+            break} 
+        case "red": {
+            color = "hsl(345, 77%, 42%)";
+            break}
+        case "eraser": {
+            color = "";
+            break}
+    }
+})
+
+// colors.forEach((colorBtn) => {
+//     colorBtn.addEventListener("click", (e) =>{
+//         e.preventDefault();
+//         console.log(e);
+//         console.log("clicked!")
+
+//     })
+// })
+
+
 
